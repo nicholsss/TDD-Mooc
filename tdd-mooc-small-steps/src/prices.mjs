88d6064a7bca69534b325161c5ee55ca09d3d0ac
Nicholas <mickyaw1995@gmail.com>
2023-01-20 13:17:26 +0200

    tcr: tests pass

--- tdd-mooc-small-steps/src/prices.mjs
+++ tdd-mooc-small-steps/src/prices.mjs
@@ -29,6 +29,8 @@ function createApp(database) {
       return new Date(dateString);
     }
   }
+
+  
   function calculateCost(age, type, date, baseCost) {
     if (type === "night") {
       return calculateCostForNightTicket(age, baseCost);
